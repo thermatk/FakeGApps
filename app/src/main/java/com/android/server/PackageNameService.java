8f0376b1320e0f3eea61f40b3d5a4343fddae856
brumsel <brumsel@losecatcher.de>
2015-11-16 23:07:52 +0100

    Fix injecting the package name service on >=5.0

--- app/src/main/java/com/android/server/PackageNameService.java
+++ app/src/main/java/com/android/server/PackageNameService.java
@@ -1,6 +1,7 @@
 package com.android.server;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.IPackageNameService;
 import android.os.RemoteException;
@@ -15,23 +16,47 @@ public class PackageNameService extends IPackageNameService.Stub {
     private final Context context;
 
     public static void inject() {
-        XposedBridge.hookAllMethods(
-            XposedHelpers.findClass("com.android.server.am.ActivityManagerService", null),
-            "main",
-            new XC_MethodHook() {
-                @Override
-                protected final void afterHookedMethod(final MethodHookParam param) {
-                    Context context = (Context) param.getResult();
-                    try {
-                        Class<?> ServiceManager = Class.forName("android.os.ServiceManager");
-                        Method addService = ServiceManager.getDeclaredMethod("addService", String.class, IBinder.class);
-                        addService.invoke(null, "packagename.service", new PackageNameService(context));
-                    } catch (Throwable ex) {
-                        XposedBridge.log("FakeGApps: Adding the package name service failed.");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            XposedBridge.hookAllMethods(XposedHelpers.findClass("android.app.ActivityThread", null),
+                    "systemMain",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam param) {
+                            final ClassLoader loader = Thread.currentThread().getContextClassLoader();
+                            XposedBridge.hookAllConstructors(
+                                    XposedHelpers.findClass("com.android.server.am.ActivityManagerService", loader),
+                                    new XC_MethodHook() {
+                                        @Override
+                                        protected void afterHookedMethod(MethodHookParam param) {
+                                            register((Context) param.getResult());
+                                        }
+                                    }
+                            );
+                        }
                     }
-                }
-            }
-        );
+            );
+        } else {
+            XposedBridge.hookAllMethods(
+                    XposedHelpers.findClass("com.android.server.am.ActivityManagerService", null),
+                    "main",
+                    new XC_MethodHook() {
+                        @Override
+                        protected final void afterHookedMethod(final MethodHookParam param) {
+                            register((Context) param.getResult());
+                        }
+                    }
+            );
+        }
+    }
+
+    private static void register(Context context) {
+        try {
+            Class<?> ServiceManager = Class.forName("android.os.ServiceManager");
+            Method addService = ServiceManager.getDeclaredMethod("addService", String.class, IBinder.class);
+            addService.invoke(null, "packagename.service", new PackageNameService(context));
+        } catch (Throwable ex) {
+            XposedBridge.log("FakeGApps: Adding the package name service failed.");
+        }
     }
 
     public PackageNameService(Context context) {
